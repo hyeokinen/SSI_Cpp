cac848be4a88f4c20371eef7e45b01b47cd2ce5f
hyeokinen <kkunh97@naver.com>
2023-05-23 16:42:28 +0900

    # 문제풀이 완료

--- SSI_Cpp/triangle.cpp
+++ SSI_Cpp/triangle.cpp
@@ -8,129 +8,51 @@ int main() {
 	// 파일 읽기 준비
 	
 	ofstream outputFile("test.txt");
-	/*ifstream in("test.txt");
-	string s1;
-	if (in.is_open()) {
-		cin >> s1;
-		std::cout << "입력 받은 문자열 :: " << s1 << std::endl;
-		outputFile << "aa";
-	}
-	else {
-		std::cout << "파일을 찾을 수 없습니다!" << std::endl;
-	}*/
-
 	string s;
 	cout << "숫자를 입력하세요." << endl;
 	getline(cin, s);
 	int num = stoi(s);
-	
-<<<<<<< HEAD
+
 	// 1번째 삼각형
 	cout << "1번째 삼각형" << endl;
-=======
-	// 1��° �ﰢ��
-	cout << "1��° �ﰢ��" << endl;
->>>>>>> d31bd19538b35e048319fb599dc998af133185cf
 	for (int i = 1; i <= num; i++)
 	{
-		for (int j = 1; j <= i; j++) 
-		{
-			cout << "*";
-			outputFile << "*";
-		}
+		for (int j = 1; j <= i; j++) cout << "*";
 		cout << endl;
-		outputFile << endl;
 	}
-<<<<<<< HEAD
 
 	cout << endl;
 	cout << endl;
 	cout << endl;
-	outputFile << endl;
-	outputFile << endl;
-	outputFile << endl;
 
 	// 2번째 삼각형
 	cout << "2번째 삼각형" << endl;
-=======
-
-	cout << endl;
-	cout << endl;
-	cout << endl;
-
-	// 2��° �ﰢ��
-	cout << "2��° �ﰢ��" << endl;
->>>>>>> d31bd19538b35e048319fb599dc998af133185cf
 	for (int i = 0; i < num; i++)
 	{
-		for (int k = 0; k < i; k++) 
-		{
-			cout << " ";
-			outputFile << " ";
-		}
-		for (int j = 0; j < num - i; j++) 
-		{
-			cout << "*";
-			outputFile << "*";
-		}
+		for (int k = 0; k < i; k++) cout << " ";
+		for (int j = 0; j < num - i; j++) cout << "*";
 		cout << endl;
-		outputFile << endl;
 	}
 
 	cout << endl;
 	cout << endl;
 	cout << endl;
-	outputFile << endl;
-	outputFile << endl;
-	outputFile << endl;
 
-<<<<<<< HEAD
 	// printf 사용
 	// 3번째 삼각형
 	cout << "3번째 삼각형" << endl;
 	for (int i = 0; i < num; i++)
 	{
-		for (int j = i; j < num; j++) 
-		{
-			printf(" ");
-			outputFile << " ";
-		}
-		for (int k = 0; k < i * 2 + 1; k++) 
-		{
-			printf("*");
-			outputFile << "*";
-		}// 별은 홀수개가 찍혀야 한다. 그러므로 2n+1
-=======
-	// printf ���
-	// 3��° �ﰢ��
-	cout << "3��° �ﰢ��" << endl;
-	for (int i = 0; i < num; i++)
-	{
 		for (int j = i; j < num; j++) printf(" ");
-		for (int k = 0; k < i * 2 + 1; k++) printf("*"); // ���� Ȧ������ ����� �Ѵ�. �׷��Ƿ� 2n+1
->>>>>>> d31bd19538b35e048319fb599dc998af133185cf
+		for (int k = 0; k < i * 2 + 1; k++) printf("*"); // 별은 홀수개가 찍혀야 한다. 그러므로 2n+1
 		printf("\n");
-		outputFile << endl;
 	}
-<<<<<<< HEAD
-	for (int i = 0; i < num-1; i++) // 위에 삼각형을 반대로 뒤집은 결과 이지만 num의 숫자보다 1이 적은 숫자로 계산
+	for (int i = 0; i < num - 1; i++) // 위에 삼각형을 반대로 뒤집은 결과 이지만 num의 숫자보다 1이 적은 숫자로 계산
 	{
 		printf("  ");
-		outputFile << "  ";
-		for (int k = 0; k < i; k++) 
-		{
-			printf(" ");
-			outputFile << " ";
-		}
-
-
-		for (int j = i * 2; j < (num - 1) * 2 - 1; j++) 
-		{
-			printf("*");
-			outputFile << "*";
-		}
+		for (int k = 0; k < i; k++) printf(" ");
+		for (int j = i * 2; j < (num - 1) * 2 - 1; j++) printf("*");
 		printf("\n");
-		outputFile << endl;
 	}
 
 	// 4번째 삼각형
@@ -156,13 +78,12 @@ int main() {
 			printf("*");
 			outputFile << "*";
 		}
-=======
+
 	for (int i = 0; i < num-1; i++) // ���� �ﰢ���� �ݴ�� ������ ��� ������ num�� ���ں��� 1�� ���� ���ڷ� ���
 	{
 		printf("  ");
 		for (int k = 0; k < i; k++) printf(" ");
 		for (int j = i * 2; j < (num-1) * 2 - 1; j++) printf("*");
->>>>>>> d31bd19538b35e048319fb599dc998af133185cf
 		printf("\n");
 		outputFile << endl;
 	}
